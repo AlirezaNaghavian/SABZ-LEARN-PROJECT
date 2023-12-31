e1fc3dbe16508030ae93a3992463f351f555ae3e
alirezangh <alireza20002485@gmail.com>
2023-12-31 11:21:37 -0800

    fix-login-section

--- public/assests/js/app.js
+++ public/assests/js/app.js
@@ -2,6 +2,8 @@ const getLgSearchBtn =document.getElementById("active-search-box");
 const getSearchBox =document.getElementById("search-box");
 const getOvralay = document.getElementsByClassName("app-overlay")[0];
 const getSwichTheme = document.getElementsByClassName("switch-theme")[0];
+const getUserProfileIcon =document.getElementById("user-profile-dropdown");
+const userProfileWrapper = document.getElementById("userProfile");
 let changeTheme = ()=>{
 document.querySelector("html").classList.toggle("dark");
 if (document.querySelector("html").classList.contains("dark")) {
@@ -24,14 +26,25 @@ let showOverlaySearchBox = ()=>{
     getOvralay.classList.toggle("hide")
     getOvralay.classList.toggle("show")
 }
+let showUserProfile = ()=>{
+    getUserProfileIcon.classList.toggle("hide")
+    getUserProfileIcon.classList.toggle("show")
+    getUserProfileIcon.classList.toggle("z-50")
+    userProfileWrapper.classList.toggle("z-50")
+    getOvralay.classList.toggle("hide")
+    getOvralay.classList.toggle("show")
+}
 let onClickOverlay = ()=>{
     getLgSearchBtn.classList.add("hide")
     getLgSearchBtn.classList.remove("show")
     getSearchBox.classList.remove("z-50")
     getOvralay.classList.add("hide")
     getOvralay.classList.remove("show")
-
+    getUserProfileIcon.classList.add("hide")
+    getUserProfileIcon.classList.remove("show")
+    getUserProfileIcon.classList.remove("z-50")
 }
+userProfileWrapper.addEventListener("click",showUserProfile)
 window.addEventListener("load",checkTheme)
 getSwichTheme.addEventListener("click",changeTheme)
 getOvralay.addEventListener("click",onClickOverlay)
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,48 +3,48 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   darkMode: "class",
   theme: {
-    screens:{
-      "xs": "320px",
-      "sm"  :`640px`,
-      "md"  :`768px`,
-      "lg"  :`1024px`,
-      "xl"  :`1280px`,
-      "2xl" : `1366px`,
+    screens: {
+      xs: "320px",
+      sm: `640px`,
+      md: `768px`,
+      lg: `1024px`,
+      xl: `1280px`,
+      "2xl": `1366px`,
     },
     extend: {
-      borderRadius:{
-        "4xl" : "2rem"
+      borderRadius: {
+        "4xl": "2rem",
       },
-      letterSpacing:{
-        "tightest": "-0.065em"
+      letterSpacing: {
+        tightest: "-0.065em",
       },
-      container:{
-        center : true,
-        padding :{
+      container: {
+        center: true,
+        padding: {
           DEFAULT: "1rem",
-          sm:"0.625rem"
-        }
+          sm: "0.625rem",
+        },
       },
-      fontFamily:{
-        "Dana" : "Dana",
-        "DanaMedium" : "Dana Medium",
-        "DanaBold" : "Dana DemiBold",
-        "MorabbaLight" : "Morabba Light",
-        "MorabbaMedium" : "Morabba Medium",
-        "Morabba_bold" : "Morabba Bold",
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana Medium",
+        DanaBold: "Dana DemiBold",
+        MorabbaLight: "Morabba Light",
+        MorabbaMedium: "Morabba Medium",
+        Morabba_bold: "Morabba Bold",
       },
-      colors:{
-        grayTheme:"#1c1c28",
-        baseColor :"#2ED573",
-        
-      }
+      colors: {
+        grayTheme: "#1c1c28",
+        baseColor: "#2ED573",
+        secondaryLight: "#4e81fb66",
+        secondary:"#4e81fb",
+      },
     },
   },
   plugins: [
-    function({addVariant}){
-      addVariant(`child`,`&>*`);
-      addVariant(`child-hover`,`&>*:hover`)
-    }
+    function ({ addVariant }) {
+      addVariant(`child`, `&>*`);
+      addVariant(`child-hover`, `&>*:hover`);
+    },
   ],
-}
-
+};
