a5bab206586017da3592769751bc841313f1b27c
alirezangh <alireza20002485@gmail.com>
2024-01-25 09:05:37 -0800

    add-aside-component

--- public/Admin-panel/man-page/assest/main.js
+++ public/Admin-panel/man-page/assest/main.js
@@ -1,2 +1,4 @@
 import HeaderTemplate from "../../../assests/components/panel-header/panel-header.js";
+import Aside from "../../../assests/components/side-bar-panel/aside.js";
+window.customElements.define("aside-tg",Aside)
 window.customElements.define("header-tg",HeaderTemplate)
