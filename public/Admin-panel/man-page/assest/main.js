6c628a5f35592649c94e2a464266afb4e8dbfd03
alirezangh <alireza20002485@gmail.com>
2024-02-03 02:53:11 -0800

    set-header-admin-info

--- public/Admin-panel/man-page/assest/main.js
+++ public/Admin-panel/man-page/assest/main.js
@@ -1,4 +1,13 @@
 import HeaderTemplate from "../../../assests/components/panel-header/panel-header.js";
 import Aside from "../../../assests/components/side-bar-panel/aside.js";
+
+
+
+
+
+
+
+
+
 window.customElements.define("aside-tg",Aside)
 window.customElements.define("header-tg",HeaderTemplate)
