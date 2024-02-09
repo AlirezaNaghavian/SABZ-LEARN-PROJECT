53e513ecfdcff240d6709bfbf0a623a7285dbec6
alirezangh <alireza20002485@gmail.com>
2024-02-09 12:14:31 -0800

    add-user-panel-list

--- public/Admin-panel/p-user-list/assest/p-user.js
+++ public/Admin-panel/p-user-list/assest/p-user.js
@@ -1,4 +1,44 @@
 import HeaderTemplate from "../../../assests/components/panel-header/panel-header.js"
 import Aside from "../../../assests/components/side-bar-panel/aside.js";
+import { getToken } from "../../../assests/components/utilities/utilities.js";
+const userTbody = document.getElementById("user-tBody")
+
+const getAllUsers = async ()=>{
+    const fetchUsers = await fetch(`http://localhost:4000/v1/users`,{
+        headers:{
+            Authorization : `Berear ${getToken()}`
+        }
+    })
+    const getUserRes = await fetchUsers.json();
+    console.log(getUserRes);
+ getUserRes.forEach((user,index)=>{
+    userTbody.insertAdjacentHTML("beforeend",`
+    
+    <tr class="grid grid-cols-10  w-full child:my-auto child:text-center  child:mx-auto  mt-8 child:text-gray-700 rounded-lg bg-gray-300   child:py-2">
+
+    <td class="w-[10%] flex justify-center text-right ml-auto my-auto" id="id">${index < 9 ? `0${index + 1}` : index + 1}</td>
+    <td class="w-[10%] flex justify-center text-center text-sm my-auto line-clamp-1 truncate"  id="name">${user.name}</td>
+    <td class="w-[10%] flex justify-center text-center text-sm my-auto line-clamp-1 truncate"  id="name">${user.username}</td>
+    <td class="w-[10%] flex justify-center text-center text-sm my-auto line-clamp-1 truncate"  id="userPhone">${user.phone}</td>
+    <td class="w-[10%] flex text-wrap flex-wrap justify-center text-center text-xs my-auto"  id="userEmail">${user.email}</td>
+    <td class="w-[10%] flex justify-center text-center text-sm my-auto line-clamp-1 truncate"  id="userRole">${user.role}</td>
+    <td class="w-[10%] flex justify-center my-auto">
+        <button type="button"  class="edit-btn btn bg-baseColor p-2 rounded-lg text-white" id="edit-btn">ویرایش</button>
+    </td>
+        <td class="w-[10%] my-auto"><button type="button"  onclick=editRole("${user._id}") class="edit-btn btn bg-secondary p-2 rounded-lg text-white" id="edit-btn">تغییر نقش</button></td>
+        <td class="w-[10%] my-auto"><button type="button"  onclick=deleteUser("${user._id}")  class="del-btn text-white bg-rose-500 p-2 rounded-lg" id="delete-btn">حذف</button></td>
+        <td class="w-[10%] my-auto"><button type="button"  onclick=banUser("${user._id}")  class="del-btn text-white bg-gray-500 p-2 rounded-lg" id="ban-btn">بن کردن</button></td>
+      
+    </tr>
+    
+    `)
+ })
+}
+window.addEventListener("load",()=>{
+    getAllUsers();
+})
+
+
+
 window.customElements.define("header-tg",HeaderTemplate)
 window.customElements.define("aside-tg",Aside);
