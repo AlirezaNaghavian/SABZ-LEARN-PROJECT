65f8d1b8dd6414f3ab05611585567c8a95f623cf
alirezangh <alireza20002485@gmail.com>
2024-02-09 13:15:45 -0800

    add-ban-user-phone

--- public/Admin-panel/p-user-list/assest/p-user.js
+++ public/Admin-panel/p-user-list/assest/p-user.js
@@ -14,38 +14,39 @@ const getAllUsers = async ()=>{
  getUserRes.forEach((user,index)=>{
     userTbody.insertAdjacentHTML("beforeend",`
     
-    <tr class="grid grid-cols-10  w-full child:my-auto child:text-center  child:mx-auto  mt-8 child:text-gray-700 rounded-lg bg-gray-300   child:py-2">
+    <tr class="grid grid-cols-9  w-full child:my-auto child:text-center  child:mx-auto  mt-8 child:text-gray-700 rounded-lg bg-gray-300   child:py-2">
 
-    <td class="w-[10%] flex justify-center text-right ml-auto my-auto" id="id">${index < 9 ? `0${index + 1}` : index + 1}</td>
-    <td class="w-[10%] flex justify-center text-center text-sm my-auto line-clamp-1 truncate"  id="name">${user.name}</td>
-    <td class="w-[10%] flex justify-center text-center text-sm my-auto line-clamp-1 truncate"  id="name">${user.username}</td>
-    <td class="w-[10%] flex justify-center text-center text-sm my-auto line-clamp-1 truncate"  id="userPhone">${user.phone}</td>
-    <td class="w-[10%] flex text-wrap flex-wrap justify-center text-center text-xs my-auto"  id="userEmail">${user.email}</td>
-    <td class="w-[10%] flex justify-center text-center text-sm my-auto line-clamp-1 truncate"  id="userRole">${user.role}</td>
-    <td class="w-[10%] flex justify-center my-auto">
-        <button type="button"  class="edit-btn btn bg-baseColor p-2 rounded-lg text-white" id="edit-btn">ویرایش</button>
+    <td class=" flex justify-center text-right ml-auto my-auto" id="id">${index < 9 ? `0${index + 1}` : index + 1}</td>
+    <td class=" flex justify-center text-center text-sm my-auto "  id="name">${user.name}</td>
+    <td class=" flex justify-center text-center text-sm my-auto "  id="name">${user.username}</td>
+    <td class=" flex justify-center text-center text-sm my-auto "  id="userPhone">${user.phone}</td>
+    <td class=" flex text-wrap flex-wrap justify-center text-center text-xs my-auto"  id="userEmail">${user.email}</td>
+    <td class=" flex justify-center text-center text-sm my-auto "  id="userRole">${user.role}</td>
+    <td class=" flex justify-center my-auto">
+        <button type="button"  class="edit-btn btn bg-secondary p-2 rounded-lg text-white" id="edit-btn">تغییر نقش</button>
     </td>
-        <td class="w-[10%] my-auto"><button type="button"  onclick=editRole("${user._id}") class="edit-btn btn bg-secondary p-2 rounded-lg text-white" id="edit-btn">تغییر نقش</button></td>
-        <td class="w-[10%] my-auto"><button type="button"  onclick=deleteUser("${user._id}")  class="del-btn text-white bg-rose-500 p-2 rounded-lg" id="delete-btn">حذف</button></td>
-        <td class="w-[10%] my-auto"><button type="button"  onclick=banUser("${user._id}")  class="del-btn text-white bg-gray-500 p-2 rounded-lg" id="ban-btn">بن کردن</button></td>
+
+        <td class=" my-auto"><button type="button"  onclick=deleteUser("${user._id}")  class="del-btn text-white bg-rose-500 p-2 rounded-lg" id="delete-btn">حذف</button></td>
+        <td class=" my-auto"><button type="button"  onclick=banUser("${user._id}")  class=" del-btn text-white bg-gray-500 p-2 rounded-lg" id="ban-btn">بن کردن</button></td>
       
     </tr>
-    
     `)
  })
 }
 
 
 const deleteUser =async (userId)=>{
+
   await Swal.fire({
     title: "آیا مطمئن هستید؟",
-    icon: "warning",
+    icon: "error",
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
     cancelButtonText: "خیر منصرف شدم",
     confirmButtonText: "بله حذف شود",
-  }).then(async (result) => {
+  })
+  .then(async (result) => {
     if (result.isConfirmed) {
       Swal.fire({
         title: "موفقیت آمیز !",
@@ -65,15 +66,55 @@ const deleteUser =async (userId)=>{
       console.log(getResData);
       location.reload();
     }
-    else{
-     await Swal.fire({
-            title: "خطا!",
-            text: " مشکلی پیش آمده لطفا دوباره تلاش کنید",
-            icon: "error",
-          });
-    }
-  });
+   
+  })
 }
+const banUser = async (userId)=>{
+    await Swal.fire({
+        title: "آیا از بن کردن کاربر مطمئن هستید؟",
+        text: "برای رفع بن کاربر باید شماره تلفن کاربر از دیتابیس پاک شود",
+        icon: "error",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        cancelButtonText: "خیر منصرف شدم",
+        confirmButtonText: "بله حذف شود",
+      })
+      .then(async (result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "موفقیت آمیز !",
+            text: "کاربر مورد نظر بن گردید",
+            icon: "success",
+          });
+          const fetchData = await fetch(
+            `http://localhost:4000/v1/users/ban/${userId}`,
+            {
+              method: "PUT",
+              headers: {
+                Authorization: `Berear ${getToken()}`,
+              },
+            }
+          );
+          const fetchDeleteData = await fetch(
+            `http://localhost:4000/v1/users/${userId}`,
+            {
+              method: "DELETE",
+              headers: {
+                Authorization: `Berear ${getToken()}`,
+              },
+            }
+          );
+          const fetBanUserRes = fetchData.json();
+          const getResData = await fetchDeleteData.json();
+          console.log(getResData);
+          console.log(fetBanUserRes);
+          location.reload();
+        }
+       
+      })
+}
+window.banUser = banUser;
 window.deleteUser = deleteUser;
 window.addEventListener("load",()=>{
     getAllUsers();
